09c4fb65e46d48a4766e6e0fcef60a23e7151496
usman ulfat <usmanulfat754@gmail.com>
2024-01-02 21:31:32 +0500

    Add project files.

---
+++ Projecttt/Data/Admission.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Projecttt.Data
+{
+    public class Admission
+    {
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50, ErrorMessage = "Input Name Limit Exceeds", MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public string RollNumber { get; set; }
+        public DateTime DOB { get; set; }
+    }
+}
---
+++ Projecttt/Model/AddContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Projecttt.Components.Pages;
+
+namespace Projecttt.Model
+{
+    public class AddContext : DbContext
+    {
+        public AddContext(DbContextOptions<AddContext> options) : base(options) //configure dbContext option 
+        {
+
+        }
+        public DbSet<Admission> Admissions { get; set; }
+        public object Admission { get; internal set; }
+    }
+}
